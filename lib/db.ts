5064fb522392405bf6b97c149ca137fd6fb1dfd0
areeyaananth09 <aryachillzzofficial@gmail.com>
2026-01-08 21:34:18 +0530

    auth process ensured and completed sucessfully

--- lib/db.ts
+++ lib/db.ts
@@ -5,5 +5,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
 }
 
+import * as schema from "./schema";
+
 const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
